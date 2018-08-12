933f9096d127b9a03a28992d4ce0998051403efa
Grant Watson <grant@triumphdesigns.org>
2018-08-12 19:01:15 -0400

    Minor Update

--- src/components/navMenu.jsx
+++ src/components/navMenu.jsx
@@ -1,24 +1,30 @@
 import '../App.css';
 import React from 'react';
-import { Router, Route } from 'react-router'
-import MainLayout from './layouts/main_layout.jsx';
-import EmptyLayout from './layouts/empty_layout.jsx';
-import Home from './pages/home.jsx';
-import About from './pages/about.jsx';
-import NotFound from './pages/not_found.jsx';
-import SignIn from './pages/sign_in.jsx';
+import { Router, Route } from 'react-router';
+import { Home } from '../pages/home';
+import { About } from '../pages/about';
+import { Contact } from '../pages/contact';
+import { Media } from '../pages/media';
+import { Portfolio } from '../pages/portfolio';
 
 const NaviRoute = () => (
-  <Router>
-    <Route component={MainLayout}>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Route>
-    <Route component={EmptyLayout}>
-      <Route path="/sign-in" component={SignIn} />
-    </Route>
-    <Route path="*" component={NotFound}/>
-  </Router>
+  <div className={"navbar-default"} style={{ backgroundColor: "#263342", height: "60px", color: "white" }}>
+    <div className={"container-fluid"}>
+        <div className={"row"}>
+            <div className={"navbar"}>
+              <Router>
+                <Route component={MainLayout}>
+                  <Route path="/" component={Home} />
+                  <Route path="/about" component={About} />
+                  <Route path="/contact" component={Contact} />
+                  <Route path="/media" component={Media} />
+                  <Route path="/portfolio" component={Portfolio} />
+                </Route>
+              </Router>
+            </div>
+        </div>
+    </div>
+  </div>
 );
 
 export default NaviRoute;
